ec8015b64d60dab668c36efc14a9bfac7e84bb5e
radumariusrobert14 <39050872+radumariusrobert14@users.noreply.github.com>
2024-07-11 22:12:26 +0300

    Update main.py

--- saltpeter/main.py
+++ saltpeter/main.py
@@ -476,7 +476,6 @@ def main():
                     p = multiprocessing.Process(target=run,\
                             args=(name,config['crons'][name],procname,running, state, commands), name=procname)
 
-                    print(config['crons'][name])
                     processlist[procname] = {}
                     processlist[procname]['cron_name'] = name
                     processlist[procname]['cron_group'] = config['crons'][name]['group']
